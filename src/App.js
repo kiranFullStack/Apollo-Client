3e6c9dcb1d9ad5212044153f27a672a4262eb79b
kiranFullStack <kiran.fullstack@outlook.com>
2019-08-04 10:07:52 +0530

    axios get

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react"
-import "./App.scss"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios("https://jsonplaceholder.typicode.com/users")
+      setData(result.data)
+    }
+    fetchData()
+  }, [])
+  console.log(data)
   return (
-    <div className="App">
-      <h1>Apollo React Client</h1>
+    <div>
+      {data.map(item => (
+        <div key={item.id}>
+          <h1>{item.name}</h1>
+        </div>
+      ))}
+      <button />
     </div>
   )
 }
-
-export default App
