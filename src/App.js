74178edf5014462b094bcdb59dd940ff9cb563c4
kiranFullStack <kiran.fullstack@outlook.com>
2019-08-04 10:14:13 +0530

    axios post

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ export default function App() {
   const [data, setData] = useState([])
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios("https://jsonplaceholder.typicode.com/users")
+      const result = await axios.post(
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+          userId: "1",
+          title: "todoTitle2",
+          body: "this is the body2"
+        }
+      )
       setData(result.data)
     }
     fetchData()
@@ -13,11 +20,11 @@ export default function App() {
   console.log(data)
   return (
     <div>
-      {data.map(item => (
+      {/* {data.map(item => (
         <div key={item.id}>
           <h1>{item.name}</h1>
         </div>
-      ))}
+      ))} */}
       <button />
     </div>
   )
